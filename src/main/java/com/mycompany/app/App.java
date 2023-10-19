902eaaf10b9a57413cdda36b06460b0f85eee4a6
Alexander Nascimento <anmc@icomp.ufam.edu.br>
2023-10-19 00:23:38 -0400

    testes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ public class App
     public static double ConverterFarenheit_Celcius(Double farenheit){
         return (farenheit - 32) * 5.0/9.0;
     }
-    public static double ConverterCelcius_Farenheit(Double celcius){
+    public static double ConverterCelcius_Farenheit(Dole celcius){
         return (celcius * 9.0/5.0) + 32;
     
 }
