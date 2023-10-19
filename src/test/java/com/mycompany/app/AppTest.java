031b1dda8831cb40139d2f7692bb7804593b64cd
Alexander Nascimento <anmc@icomp.ufam.edu.br>
2023-10-19 00:12:56 -0400

    testes

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -9,6 +9,6 @@ public class AppTest
 {
     @Test
     public void FailureTesteFarenheit_Celcius(){
-        assertEquals(0.0, App.ConverterFarenheit_Celcius(32.0), 0.0);
+        assertEquals(0.5, App.ConverterFarenheit_Celcius(32.0), 0.0);
     }
 }
