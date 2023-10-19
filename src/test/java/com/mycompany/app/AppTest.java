8a39f85eba9d707a9a5afc6787869571153ff063
Alexander Nascimento <anmc@icomp.ufam.edu.br>
2023-10-19 00:04:55 -0400

    testes

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,7 +1,7 @@
 package com.mycompany.app;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -9,6 +9,6 @@ public class AppTest
 {
     @Test
     public static void FailureTesteFarenheit_Celcius(){
-        assertEquals(32.0, App.ConverterFarenheit_Celcius(1.0), 0.0);
+        assertEquals(32.0, App.ConverterFarenheit_Celcius(0.0), 0.0);
     }
 }
