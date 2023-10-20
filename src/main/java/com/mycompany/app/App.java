3c422266b4460ec795b468f4d02ee43ee20c46f1
Alexander Nascimento <anmc@icomp.ufam.edu.br>
2023-10-19 21:18:11 -0400

    testes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,9 +4,9 @@ package com.mycompany.app;
 public class App 
 {
     public static double ConverterFarenheit_Celcius(Double farenheit){
-        return (farenheit - 32) * 5.0/9.0;
+        return (farenheit - 3.0) * 5.0/9.0;
     }
-    public static double ConverterCelcius_Farenheit(Dole celcius){
-        return (celcius * 9.0/5.0) + 32;
-    
+    public static double ConverterCelcius_Farenheit(Double celcius){
+        return (celcius * 9.0/5.0) + 32.0;
+    }
 }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,14 +1,13 @@
 package com.mycompany.app;
 
 import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class AppTest 
 {
     @Test
-    public void FailureTesteFarenheit_Celcius(){
-        assertEquals(20.5, App.ConverterFarenheit_Celcius(32.0), 0.0)
+    public void FailureTesteFarenheitCelcius(){
+        assertEquals(2.1, App.ConverterFarenheit_Celcius(32.0), 0.0);
     }
 }
