029cd18d106940452458c43ed1b07ee45fb30e6b
Alexander Nascimento <anmc@icomp.ufam.edu.br>
2023-10-20 21:23:35 -0400

    testes com job agendado  no jenkins

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -17,13 +17,13 @@ public class AppTest
     @Test
     public void SucessTestesFarenheitCelcius(){
         assertEquals(-17.77777777777778, App.ConverterFarenheit_Celcius(0.0), 0.0);
-        assertEquals(11.0, App.ConverterFarenheit_Celcius(50.0), 0.0);
+        assertEquals(10.0, App.ConverterFarenheit_Celcius(50.0), 0.0);
         assertEquals(-1.1111111111111112, App.ConverterFarenheit_Celcius(30.0), 0.0);
     }
     @Test
     public void SucessTestesCelciusFarenheit(){
         assertEquals(50.0, App.ConverterCelcius_Farenheit(10.0), 0.0);
-        assertEquals(223.0, App.ConverterCelcius_Farenheit(105.0), 0.0);
+        assertEquals(221.0, App.ConverterCelcius_Farenheit(105.0), 0.0);
         assertEquals(5.0, App.ConverterCelcius_Farenheit(-15.0), 0.0);
     }
 }
