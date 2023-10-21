61c85a867ea816c3ff874c243a87c85c3cdea7e7
Alexander Nascimento <anmc@icomp.ufam.edu.br>
2023-10-20 20:29:02 -0400

    testes com falhas em jenkins

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -8,10 +8,10 @@ public class AppTest
 {
     @Test
     public void FailureTesteFarenheitCelcius(){
-        assertEquals(32.1, App.ConverterFarenheit_Celcius(0.0),0.0);
+        assertEquals(0.0, App.ConverterFarenheit_Celcius(32.0),0.0);
     }
     @Test
     public void FailureTesteCelciusFarenheit(){
-        assertEquals(0.0, App.ConverterFarenheit_Celcius(55.0),0.0);
+        assertEquals(32.0, App.ConverterFarenheit_Celcius(0.0),0.0);
     }
 }
